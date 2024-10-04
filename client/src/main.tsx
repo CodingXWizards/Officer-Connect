287e156be46bbbc7c5d85dd1e64673fae5ae2239
Raja Singh <rajasgh18@gmail.com>
2024-10-04 10:47:43 +0530

    feat: added redux-toolkit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import '@/index.css'
+import App from '@/App.tsx'
+import { store } from '@/store/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
