233a0dacb2abfab358237c253deb60c47a609079
Raja Singh <rajasgh18@gmail.com>
2024-10-04 10:49:25 +0530

    feat: user type

---
+++ client/src/types/user.ts
@@ -0,0 +1,14 @@
+export enum DesignationType {
+    SP = 'SP',
+    ADDITIONAL_SP = 'ADDITIONAL_SP',
+    DSP = 'DSP',
+    TI = 'TI'
+}
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    designation: DesignationType
+}
